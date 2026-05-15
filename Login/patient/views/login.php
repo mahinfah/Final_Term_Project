a7cae1f9488e83d415835e710dd3e553bc71f0dd
SakibRahmanAIUB <23-51093-1@student.aiub.edu>
2026-05-16 03:54:27 +0600

    twenty nine

--- Login/patient/views/login.php
+++ Login/patient/views/login.php
@@ -137,7 +137,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <input type="password" name="password" required>
         </div>
 
-        <button type="submit">Login</button>
+        <button type="submit" name="login">Login</button>
     </form>
 
     <div class="register-links">
