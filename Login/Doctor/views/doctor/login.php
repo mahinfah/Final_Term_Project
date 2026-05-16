702995f3c0e1e74565be4ca63f8cc3843f8e097a
22-49437-3 <zahidhossain91234@gmail.com>
2026-05-16 18:35:53 +0600

    09

---
+++ Login/Doctor/views/doctor/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head><title>Doctor Login</title><link rel="stylesheet" href="assets/css/style.css"></head>
+<body>
+<div class="container">
+    <h2>Doctor Login</h2>
+    <form method="post">
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+        <?php if(isset($error)) echo "<p class='error'>$error</p>"; ?>
+    </form>
+</div>
+</body>
+</html>
