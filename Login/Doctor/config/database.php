b77720187d58f989d3d341ac24bdbe7c040ef33b
22-49437-3 <zahidhossain91234@gmail.com>
2026-05-17 13:13:04 +0600

    24

--- Login/Doctor/config/database.php
+++ Login/Doctor/config/database.php
@@ -11,39 +11,13 @@ class Database {
         try {
             $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
             if ($this->conn->connect_error) {
-                throw new Exception("Connection failed: " . $this->conn->connect_error);
+                die("Connection failed: " . $this->conn->connect_error);
             }
-            $this->conn->set_charset("utf8");
+            $this->conn->set_charset("utf8mb4");
         } catch(Exception $e) {
-            die("Database error: " . $e->getMessage());
+            die("Connection error: " . $e->getMessage());
         }
         return $this->conn;
-    }
-
-    public function select($sql, $params = [], $types = "") {
-        $stmt = $this->conn->prepare($sql);
-        if ($params) {
-            $stmt->bind_param($types, ...$params);
-        }
-        $stmt->execute();
-        return $stmt->get_result();
-    }
-
-    public function execute($sql, $params = [], $types = "") {
-        $stmt = $this->conn->prepare($sql);
-        if ($params) {
-            $stmt->bind_param($types, ...$params);
-        }
-        return $stmt->execute();
-    }
-
-    public function insert($sql, $params = [], $types = "") {
-        $stmt = $this->conn->prepare($sql);
-        if ($params) {
-            $stmt->bind_param($types, ...$params);
-        }
-        $stmt->execute();
-        return $stmt->insert_id;
     }
 }
 ?>
